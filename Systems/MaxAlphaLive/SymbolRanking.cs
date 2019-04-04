e3ef261afcc22eab484a1636dbb3daf7695ced7d
jrathgeber <jrathgeber@yahoo.com>
2019-04-04 06:13:24 -0400

    nl

--- Systems/MaxAlphaLive/SymbolRanking.cs
+++ Systems/MaxAlphaLive/SymbolRanking.cs
@@ -519,7 +519,7 @@ public class MySymbolScript : MySymbolScriptBase
 						settings.StopLossType = TargetPriceType.RelativePrice;  
 																		
 						OutputMessage("Going Long");
-						OpenPosition(settings);
+						//OpenPosition(settings);
 						tradedTodayLong = true;
 										
 						//OpenPosition(PositionType.Long, OrderType.Market);
@@ -727,7 +727,7 @@ public class MySymbolScript : MySymbolScriptBase
 		TradingSystem.dayTradesLeft = TradingSystem.dayTradesLeft - 1;
 		
 		// Send conf
-		// sendMail("Trade: " + trade.TransactionType + " " + trade.Size + " " + this.Symbol + " at " + this.Close.Current , " Details [" + trade.Description + "]"  );	
+		sendMail("Trade: " + trade.TransactionType + " " + trade.Size + " " + this.Symbol + " at " + this.Close.Current , " Details [" + trade.Description + "]"  );	
 		
 	}
 
